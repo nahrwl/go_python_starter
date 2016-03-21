181f7e3f7f622fc98c45cea8cf6d32a35573a24f
Jude Hungerford <edward.h@gmx.net>
2016-03-21 21:02:22 +1100

    add comment about python2 vs 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# python3
+# Compatible with python2 and python3 - delete this line to use python2
 
 import sys
 
@@ -6,6 +6,10 @@ from Bot.go import Go
 
 from Bot.bot import Bot
 
-bot = Bot()
-game = Go()
-game.run(bot)
+
+def __main__():
+    bot = Bot()
+    game = Go()
+    game.run(bot)
+
+__main__()
